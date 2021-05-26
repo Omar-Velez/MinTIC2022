f3d3b50571ba7068370bc58e931c247a5bb08363
Omar Velez <omar.velez@gmail.com>
2021-05-26 15:43:08 -0500

    agregando arreglo en sueldoDescuentos.py

--- Perfeccionamiento/sueldoDescuentos.py
+++ Perfeccionamiento/sueldoDescuentos.py
@@ -1,4 +1,4 @@
-lista_datos = [int(d) for d in input().split()]
+lista_datos = [float(valor) for valor in input().split()]
 
 salarioBase=lista_datos[0]
 horasExtras=lista_datos[1]
@@ -8,15 +8,15 @@ bonificacion=lista_datos[2]
 horaTrabajo=salarioBase/171
 
 # Las horas extras se liquidan con un recargo del 17% sobre el valor de una hora normal.
-liqHorasExtras=horaTrabajo*0.17
+liqHorasExtras=(horasExtras*horaTrabajo)*1.17
 
 # Debido a buen desempeño de un empleado la empresa ocasionalmente otorga bonificaciones de 8.8% del salario base.
 liqBonificacion=0
 if bonificacion==1:
-    liqBonificacion=salarioBase*.088
+    liqBonificacion=salarioBase*0.088
 
 # El salario total antes de descuentos se calcula como la suma del salario base, más el valor de las horas extras,
 # más las bonificaciones (si las hay).
-salarioTotal=round((salarioBase+(liqHorasExtras*horasExtras)+liqBonificacion),1)
-
-print(salarioTotal,salarioTotal*0.89)
+salarioTotal=salarioBase+liqBonificacion+liqHorasExtras
+salarioDescuento=salarioTotal*0.89
+print(round(salarioTotal,1),round(salarioDescuento,1))
