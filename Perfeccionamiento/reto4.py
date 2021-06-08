490c19be0879f2570b6056b6fe553055d921887c
Omar Velez <omar.velez@gmail.com>
2021-06-08 18:04:03 -0500

    reto 4

--- Perfeccionamiento/reto4.py
+++ Perfeccionamiento/reto4.py
@@ -1,1 +1,8 @@
+cantidadMonedas, memoria = 5,3 #[int(valor) for valor in input().split()]
+monedas=[int(valor) for valor in input().split()]
 
+monedas.reverse()
+print(monedas)
+
+# 11 4
+# 1 2 3 4 5 6 7 8 9
